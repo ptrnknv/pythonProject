84ab073ed5e231df1e77185844a2ad72192839ff
petr <pnikono24@gmail.com>
2022-09-12 16:03:33 +0300

    [add] added 1 file

--- SecurePasswordGenerator.py
+++ SecurePasswordGenerator.py
@@ -1,22 +1,66 @@
 # Подключаем модуль random
 
-import random
+from random import choice, shuffle
+
+# Функция генерирующая пароли
+
+def generate_password(length, chars):
+    password = ''
+    i = 0
+    count = 1
+    while i <= length - count:
+        for j in range(len(chars) - amb):       # Удаляем ненужные символы
+            if amb == 1:
+                for c in chars[-1]:
+                    chars[j] = chars[j].replace(c, '')
+
+            password += choice(chars[j])
+            count += 1
+
+    password = list(password)
+    shuffle(password)
+
+    return ''.join(password)
 
 # Строковые константы
 
-DIGITS = 0123456789
+DIGITS = '0123456789'
 LOWERCASE_LETTERS = 'abcdefghijklmnopqrstuvwxyz'
 UPPERCASE_LETTERS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 PUNCTUATION = '!#$%&*+-=?@^_.'
+AMBIGUOUS = 'il1Lo0O'
 
-chars = ''    # Переменная chars = '', которая содержит все символы, которые могут быть в генерируемом пароле.
+chars = []    # Переменная chars = '' - содержит все символы, которые могут быть в генерируемом пароле.
 
 # Опредеялем переменные пользовательского ввода
 
-amount_pass = input('Укажите количество паролей для генерации: ')
-len_pass = input('Укажите длину одого пароля: ')
+amount_pass = int(input('Укажите количество паролей для генерации: '))
+len_pass = int(input('Укажите длину одного пароля: '))
 digit_pass = input('В пароле должны быть цифры?: ')
 low_case_pass = input('Включать строчные буквы?: ')
 up_case_pass = input('Включать прописные буквы? ')
 chs_pass = input('Вкючать символы: "!#$%&*+-=?@^_."? : ')
-ambchrs_pass = input('Вкючать неоднозначные символы: "il1Lo0O"? : ')
+ambchrs_pass = input('Искючать неоднозначные символы: "il1Lo0O"? : ')
+
+# Добавляем необходимые символы
+
+if digit_pass == 'y':
+    chars.append(DIGITS)
+if low_case_pass == 'y':
+    chars.append(LOWERCASE_LETTERS)
+if up_case_pass == 'y':
+    chars.append(UPPERCASE_LETTERS)
+if chs_pass == 'y':
+    chars.append(PUNCTUATION)
+if ambchrs_pass == 'y':  # Добавляем символы, которые нужно исключить
+    amb = 1
+    chars.append(AMBIGUOUS)
+else:
+    amb = 0
+
+passwords = []
+
+for _ in range(amount_pass):
+    passwords.append(generate_password(len_pass, chars))
+
+print(passwords)
