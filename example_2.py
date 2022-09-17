d540998fd9f03a2eb667e17cdd63341a2c1a1aa6
petr <pnikono24@gmail.com>
2022-09-17 13:38:18 +0300

    [upd] change 1 file

--- example_2.py
+++ example_2.py
@@ -1,9 +1,25 @@
-n = '5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 555	'.split()
-count = 1
+# 8
+# 17
+# 33
+# 56
+# 70
+# 66
+# 50
+# 20
+# 38
+# 1400
+n = int(input())
+lst = []
+for _ in range(n):
+    lst.append(int(input()))
+num = int(input())
+flag = False
+for i in range(len(lst)):
+    for j in lst[i:]:
+        if lst[i] * j == num:
+            flag = True
 
-for i in range(len(n) - 1):
-    if n[i] != n[i+1]:
-        count += 1
-
-
-print(count)
+if flag:
+    print('ДА')
+else:
+    print('НЕТ')
