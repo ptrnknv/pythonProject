14f6a4b21167143c7b4de03546f12ecf6008b4f0
petr <pnikono24@gmail.com>
2022-09-19 15:59:12 +0300

    [upd] change 1 file

--- example_2.py
+++ example_2.py
@@ -1,15 +1,12 @@
-from math import factorial
+s = 'f g g g d r t'.split()
+repeated = []
+lst = [s[0]]
 
+for i in range(1, len(s)):
+    print(lst)
+    if s[i-1] != s[i]:
+        lst.append([s[i]])
+    else:
+        lst[-1].append(s[i])
 
-def pascal(n):
-    list = []
-    for i in range(n + 1):
-        m = factorial(n) / (factorial(i) * factorial(n - i))
-        list.append(int(m))
-
-    return list
-
-
-for i in range(int(input())):
-    print(pascal(i))
-# [1, 3, 3, 1]
+print(lst)
