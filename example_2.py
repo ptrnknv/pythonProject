39268ef3f3eae408dc625acf916363a0d0528e7e
petr <pnikono24@gmail.com>
2022-09-20 16:05:07 +0300

    [upd] change 1 file

--- example_2.py
+++ example_2.py
@@ -1,10 +1,11 @@
-def chunked(s, n):
-  lst = []
-  for i in range(0, len(s), n):
-    lst.append(s[i:i+n])
+def make_sublist(s):
+  lst = [[]]
+  for n in range(1, len(s) + 1):
+    for i in range(0, len(s) + 1 - n):
+      lst.append(s[i:i + n])
   print(lst)
 
-s = input().split()
-n = int(input())
 
-chunked(s, n)
+s = '1 2 3 0'.split()
+
+make_sublist(s)
