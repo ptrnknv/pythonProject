47960f05785b5fb1ecf6425cf34bf4e8bd424da1
petr <pnikono24@gmail.com>
2022-09-20 18:47:48 +0300

    [upd] change 1 file

--- example_2.py
+++ example_2.py
@@ -1,17 +1,18 @@
-n = int(input())
-m = int(input())
-matrix = []
-for i in range(n):
-    temp = [input() for _ in range(m)]
-    matrix.append(temp)
+def print_matrix(matrix, n, width=1):
+  for r in range(n):
+    for c in range(n):
+      print(str(matrix[r][c]).ljust(width), end=' ')
+    print()
 
-for j in range(len(matrix)):
-    print(*matrix[j])
-print()
-matrix = []
-for i in range(m):
-    temp = [input() for _ in range(n)]
-    matrix.append(temp)
 
-for j in range(len(matrix)):
-    print(*matrix[j])
+n = 3
+matrix = []
+c = 1
+for r in range(1, n + 1):
+  row = []
+  for i in range(c, c + n):
+    row.append(i)
+    c = i + 1
+  matrix.append(row)
+
+print_matrix(matrix, n)
