a602d75b8312363c34c07ba4dc320db5194f39fd
petr <pnikono24@gmail.com>
2022-09-23 09:12:10 +0300

    [upd] change 1 file

--- example_2.py
+++ example_2.py
@@ -1,11 +1,21 @@
-n = int(input())
-matrix = [input().split() for _ in range(n)]
+xy = 'f3'
+y = '87654321'.index(xy[1])
+x = 'abcdefgh'.index(xy[0])
+matrix = []
+for i in range(8):
+    for j in range(8):
+        point = '.'
+        matrix.append([point] * 8)
 
-for i in range(n):
-    for j in range(n):
-        if i == j:
-            matrix[i][j], matrix[n - 1 - j][i] = matrix[n - 1 - j][i], matrix[i][j]
+for i in range(8):
+    for j in range(8):
+        if i == y and j == x:
+            matrix[i][j] = 'N'
+        inx = (x - j) * (y - i)
+        if inx == 2 or inx == -2:
+            matrix[i][j] = '*'
 
-for k in range(n):
+
+for k in range(8):
     print(*matrix[k], end='')
     print()
