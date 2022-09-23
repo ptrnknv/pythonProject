410e6851b7464647ebe58a5269b0fbc875d95cfa
petr <pnikono24@gmail.com>
2022-09-23 11:18:50 +0300

    [upd] change 1 file

--- example_2.py
+++ example_2.py
@@ -1,21 +1,14 @@
-inp = '3 4'.split()
-n = int(inp[0])
-m = int(inp[1])
-matrix = []
+n = 3
+m = 4
+board = [['0'] * m for _ in range(n)]
+last = 0
 
 for i in range(n):
-    for j in range(m):
-        matrix.append(['.'] * m)
+    for j in range(1, m + 1):
+        last += 1
+        board[i][j-1] = last
 
-for i in range(n):
-    for j in range(m):
-        if i % 2 !=0:
-            if j % 2 != 0:
-                matrix[i][j] = '*'
-        else:
-            if j % 2 == 0:
-                matrix[i][j] = '*'
-
-for k in range(n):
-    print(*matrix[k], end='')
+for row in board:
+    for col in row:
+        print(str(col).ljust(3), end='')
     print()
