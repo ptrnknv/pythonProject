8399f3f713cca21e30a1c52e5cbdb5b0c49daf7a
petr <pnikono24@gmail.com>
2022-09-23 10:06:01 +0300

    [upd] change 1 file

--- example_2.py
+++ example_2.py
@@ -1,32 +1,21 @@
-
-n = int(input())
-
-matrix = [input().split() for i in range(n)]
-has = []
-samp = []
-flag = False
+inp = '3 4'.split()
+n = int(inp[0])
+m = int(inp[1])
+matrix = []
 
 for i in range(n):
-    c, r, m, s = 0, 0, 0, 0
-    lastC = c
-    lastR = r
-    for j in range(n):
-        c += int(matrix[j][i])
-        r += int(matrix[i][j])
-        m += int(matrix[i][j])
-        s += int(matrix[i][n - j - 1])
-        has.append(int(matrix[i][j]))
+    for j in range(m):
+        matrix.append(['.'] * m)
 
-    if c == r and m == s and r == m and c == s:
-        flag = True
-    else:
-        flag = False
-        break
+for i in range(n):
+    for j in range(m):
+        if i % 2 !=0:
+            if j % 2 != 0:
+                matrix[i][j] = '*'
+        else:
+            if j % 2 == 0:
+                matrix[i][j] = '*'
 
-for i in range(min(has), n ** 2 + 1):
-    samp.append(i)
-has.sort()
-if has == samp and flag:
-    print('YES')
-else:
-    print('NO')
+for k in range(n):
+    print(*matrix[k], end='')
+    print()
