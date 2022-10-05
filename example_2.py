9261a2de8223d29b81783a05f7b1e7500d67535c
petr <pnikono24@gmail.com>
2022-10-05 11:28:12 +0300

    [upd] change 1 file

--- example_2.py
+++ example_2.py
@@ -1,13 +1,20 @@
-info = {
-    'CS101': ['3004', 'Хайнс', '8:00'],
-    'CS102': ['4501', 'Альварадо', '9:00'],
-    'CS103': ['6755', 'Рич', '10:00'],
-    'NT110': ['1244', 'Берк', '11:00'],
-    'CM241': ['1411', 'Ли', '13:00'],
+numbers = {
+    1: '.,?!:',
+    2: 'ABC',
+    3: 'DEF',
+    4: 'GHI',
+    5: 'JKL',
+    6: 'MNO',
+    7: 'PQRS',
+    8: 'TUV',
+    9: 'WXYZ',
+    0: ' ',
 }
 
-n = 'CS101'
+s = list('Hello, World!'.upper())
 
-print(f"{n}: {info[n][0]}, {info[n][1]}, {info[n][2]}")
-
-#CS101: 3004, Хайнс, 8:00
+for i in range(len(s)):
+    for number in numbers.items():
+        for j in range(len(number[1])):
+            if s[i] == number[1][j]:
+                print(str(number[0])*(j+1), end='')
