d08465fd974520bb055039a54244387423d0b126
petr <pnikono24@gmail.com>
2022-10-26 06:37:23 +0300

    [upd] change 1 file

--- example_2.py
+++ example_2.py
@@ -1,19 +1,18 @@
-def func_apply(func, items):
-    res = []
-    for item in items:
-        res.append(func(item))
-    return res
+def read_csv():
+    with open('/home/petr/Downloads/data.csv', 'r', encoding='utf-8') as file:
+        res = []
+        keys = [el.rstrip() for el in file.readline().split(',')]
+        for values in file.readlines():
+            value = [el.rstrip() for el in values.split(',')]
+            res.append(dict(zip(keys, value)))
+        return res
 
 
-def add3(x):
-    return x + 3
+print(read_csv())
 
+# name,address,age
+# George,4312 Abbey Road,22
+# John,54 Love Ave,21
 
-def mul7(x):
-    return x * 7
-
-
-print(func_apply(mul7, [1, 2, 3, 4, 5, 6]))
-print(func_apply(add3, [1, 2, 3, 4, 5, 6]))
-print(func_apply(str, [1, 2, 3, 4, 5, 6]))
+# [{'name': 'George', 'address': '4312 Abbey Road', 'age': '22'}, {'name': 'John', 'address': '54 Love Ave', 'age': '21'}]
 
