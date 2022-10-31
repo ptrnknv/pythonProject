0a98ec53227fbde3b65de1afe6dd46b20ca8318b
petr <pnikono24@gmail.com>
2022-10-31 06:58:37 +0300

    [upd] change 1 file

--- example_2.py
+++ example_2.py
@@ -1,18 +1,18 @@
-def read_csv():
-    with open('/home/petr/Downloads/data.csv', 'r', encoding='utf-8') as file:
-        res = []
-        keys = [el.rstrip() for el in file.readline().split(',')]
-        for values in file.readlines():
-            value = [el.rstrip() for el in values.split(',')]
-            res.append(dict(zip(keys, value)))
-        return res
+def spell(*args):
+    dt = [(word[0], len(word)) for word in args]
+    res = {}
+    for k, v in dt:
+        res[k.lower()] = res.get(k.lower(), v) if res.get(k.lower(), v) > v else v
+    return res
 
 
-print(read_csv())
+words = ['россия', 'Австрия', 'австралия', 'РумыниЯ', 'Украина', 'КИТай', 'УЗБЕКИСТАН']
 
-# name,address,age
-# George,4312 Abbey Road,22
-# John,54 Love Ave,21
+print(spell(*words))
+print(spell('Математика', 'История', 'химия', 'биология', 'Информатика'))
+words = ['fruit', 'football', 'February', 'forest', 'Family']
+print(spell(*words))
 
-# [{'name': 'George', 'address': '4312 Abbey Road', 'age': '22'}, {'name': 'John', 'address': '54 Love Ave', 'age': '21'}]
-
+# {'р': 7, 'а': 9, 'у': 10, 'к': 5}
+# {'м': 10, 'и': 11, 'х': 5, 'б': 8}
+# {'f': 8}
