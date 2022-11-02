37e16cf1d713497cdaaa209fd5f06f386b026daa
petr <pnikono24@gmail.com>
2022-11-02 12:20:40 +0300

    [upd] change 1 file

--- example_2.py
+++ example_2.py
@@ -1,18 +1,15 @@
-def spell(*args):
-    dt = [(word[0], len(word)) for word in args]
-    res = {}
-    for k, v in dt:
-        res[k.lower()] = res.get(k.lower(), v) if res.get(k.lower(), v) > v else v
-    return res
-
-
-words = ['россия', 'Австрия', 'австралия', 'РумыниЯ', 'Украина', 'КИТай', 'УЗБЕКИСТАН']
-
-print(spell(*words))
-print(spell('Математика', 'История', 'химия', 'биология', 'Информатика'))
-words = ['fruit', 'football', 'February', 'forest', 'Family']
-print(spell(*words))
-
-# {'р': 7, 'а': 9, 'у': 10, 'к': 5}
-# {'м': 10, 'и': 11, 'х': 5, 'б': 8}
-# {'f': 8}
+ls = [input().split('@')[0] for _ in range(int(input()))]
+dt = {}
+for el in ls:
+    if el[-1].isdigit():
+        dt[el[:-1]] = dt.get(el[:-1], []) + [el[-1]]
+    else:
+        dt[el] = dt.get(el, [])
+for _ in range(int(input())):
+    inp = input()
+    if inp in dt.keys():
+        dt[inp].append(int(dt[inp][-1])+1 if dt[inp] else 1)
+        print(inp + str(int(dt[inp][-1])) + '@beegeek.bzz' if dt[inp] else inp + '1' + '@beegeek.bzz')
+    else:
+        dt[inp] = []
+        print(inp + '@beegeek.bzz')
