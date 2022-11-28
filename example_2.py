27768812277bd69f2f8fdc73d732f0366f4fc4a0
petr <pnikono24@gmail.com>
2022-11-28 11:28:08 +0300

    [upd] change 1 file

--- example_2.py
+++ example_2.py
@@ -1,23 +1,16 @@
 import csv
 
 
-def condense_csv(file_name, id_name):
-    dt = {}
-    with open(file_name, 'r', encoding='utf-8') as file:
-        for line in csv.reader(file):
-            dt[line[0]] = dt.get(line[0], []) + [[line[1], line[2]]]
-    ls = []
-    for el in dt.items():
-        temp = {id_name: el[0]}
-        for data in el[1]:
-            temp[data[0]] = data[1]
-        ls.append(temp)
-
-    with open('condensed.csv', 'w', encoding='utf-8') as file:
-        columns = ls[0].keys()
-        writer = csv.DictWriter(file, fieldnames=columns)
-        writer.writeheader()
-        writer.writerows(ls)
+def key_func(grade):
+    num, letter = grade.split('-')
+    return int(num), letter
 
 
-condense_csv('data.csv', id_name='ID')
+with open('student_counts.csv', 'r', encoding='utf-8') as file:
+    reader = csv.DictReader(file)
+    columns = ['year'] + sorted(reader.fieldnames[1:], key=key_func)
+    rows = list(reader)
+with open('sorted_student_counts.csv', 'w') as file:
+    writer = csv.DictWriter(file, fieldnames=columns)
+    writer.writeheader()
+    writer.writerows(rows)
