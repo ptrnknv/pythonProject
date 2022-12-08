99ef47c9bbdbe576f99f873c6379b5a306551879
petr <pnikono24@gmail.com>
2022-12-08 13:48:37 +0300

    [upd] change 1 file

--- example_2.py
+++ example_2.py
@@ -1,7 +1,25 @@
-import string
+from collections import namedtuple
 
+User = namedtuple('User', ['name', 'surname', 'email', 'plan'])
 
-eng_alpha = string.ascii_lowercase
-given_alpha = input()
-st = input()
-print(st.lower().translate(st.maketrans(eng_alpha, given_alpha)))
+users = [User('Mary', 'Griffin', 'sonnen@yahoo.com', 'Basic'),
+         User('Brenda', 'Young', 'retoh@outlook.com', 'Silver'),
+         User('Kathleen', 'Lyons', 'balchen@att.net', 'Gold'),
+         User('Pamela', 'Hicks', 'corrada@sbcglobal.net', 'Silver'),
+         User('William', 'Townsend', 'kosact@verizon.net', 'Gold'),
+         User('Clayton', 'Morris', 'berserk@yahoo.com', 'Silver'),
+         User('Dorothy', 'Dennis', 'sequin@live.com', 'Gold'),
+         User('Tyler', 'Walker', 'noahb@comcast.net', 'Basic'),
+         User('Joseph', 'Moore', 'ylchang@sbcglobal.net', 'Silver'),
+         User('Kenneth', 'Richardson', 'tbusch@me.com', 'Bronze'),
+         User('Stephanie', 'Bush', 'neuffer@live.com', 'Gold'),
+         User('Gregory', 'Hughes', 'juliano@att.net', 'Basic'),
+         User('Tracy', 'Wallace', 'sblack@me.com', 'Silver'),
+         User('Russell', 'Smith', 'isaacson@comcast.net', 'Bronze'),
+         User('Megan', 'Patterson', 'hoangle@outlook.com', 'Basic')]
+
+for user in sorted(users, key=lambda x: (('Gold', 'Silver', 'Bronze', 'Basic').index(x[3]), x[2])):
+    print(f"""{user.name} {user.surname}
+  Email: {user.email}
+  Plan: {user.plan}""")
+    print()
