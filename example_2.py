acd170e11c3645a08eb0e2d2015ee7b7b62266d2
petr <pnikono24@gmail.com>
2022-12-09 15:54:47 +0300

    [upd] change 1 file

--- example_2.py
+++ example_2.py
@@ -1,11 +1,11 @@
-import csv
-from collections import namedtuple
-from datetime import datetime
+from collections import defaultdict
 
-with open('meetings.csv', 'r', encoding='utf8') as file:
-    guests = sorted(csv.DictReader(file), key=lambda x: (datetime.strptime(x['meeting_date'], '%d.%m.%Y'),
-                                                         datetime.strptime(x['meeting_time'], '%H:%M')))
-    for guest in guests:
-        Guest = namedtuple('Guest', ['surname', 'name'])
-        temp = Guest(guest['surname'], guest['name'])
-        print(*temp)
+data = [('Books', 1343), ('Books', 1166), ('Merch', 616), ('Courses', 966), ('Merch', 1145), ('Courses', 1061), ('Books', 848), ('Courses', 964), ('Tutorials', 832), ('Merch', 642), ('Books', 815), ('Tutorials', 1041), ('Books', 1218), ('Tutorials', 880), ('Books', 1003), ('Merch', 951), ('Books', 920), ('Merch', 729), ('Tutorials', 977), ('Books', 656)]
+
+total = defaultdict(int)
+
+for item, cost in sorted(data):
+    total[item] += cost
+
+for k, v in total.items():
+    print(k, ': $', v, sep='')
