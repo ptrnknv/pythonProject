ff3e2236aa6819cdb402553c852003080ea27f13
petr <pnikono24@gmail.com>
2022-12-16 11:23:28 +0300

    [upd] change 1 file

--- example_2.py
+++ example_2.py
@@ -1,20 +1,25 @@
-from collections import ChainMap, Counter
+from calendar import day_name
+import locale
+locale.setlocale(locale.LC_ALL, 'ru_RU.UTF-8')
+days = {i: d for i, d in enumerate(map(str.title, day_name), 1)}
 
-bread = {'булочка с кунжутом': 15, 'обычная булочка': 10, 'ржаная булочка': 15}
-meat = {'куриный бифштекс': 50, 'говяжий бифштекс': 70, 'рыбный бифштекс': 40}
-sauce = {'сливочно-чесночный': 15, 'кетчуп': 10, 'горчица': 10, 'барбекю': 15, 'чили': 15}
-vegetables = {'лук': 10, 'салат': 15, 'помидор': 15, 'огурцы': 10}
-toppings = {'сыр': 25, 'яйцо': 15, 'бекон': 30}
 
-ingredients = ChainMap(bread, meat, sauce, vegetables, toppings)
-order = Counter(input().split(','))
-max_len = []
-total = 0
-for ingredient, amount in sorted(order.items()):
-    ingredient_string = f'{ingredient.ljust(len(max(order, key=len)))} x {amount}'
-    print(ingredient_string)
-    max_len.append(len(ingredient_string))
-    total += ingredients[ingredient] * amount
-total_string = f'ИТОГ: {total}р'
-max_len.append(len(total_string))
-print(''.ljust(max(max_len), '-'), total_string, sep='\n')
+def get_weekday(number):
+    try:
+        if type(number) != int:
+            raise TypeError('Аргумент не является целым числом')
+        elif number not in range(1, 8):
+            raise ValueError('Аргумент не принадлежит требуемому диапазону')
+        else:
+            return days[number]
+    except TypeError as errT:
+        raise errT
+    except ValueError as errV:
+        raise errV
+
+
+try:
+    print(get_weekday(8))
+except ValueError as err:
+    print(err)
+    print(type(err))
