3e7a9bd9e57edbd5708bef8a816ff2d37badddd3
petr <pnikono24@gmail.com>
2022-12-21 15:35:31 +0300

    [upd] change 1 file

--- example_2.py
+++ example_2.py
@@ -1,19 +1,13 @@
-import time
-
-start_time = time.perf_counter()
-def tribonacci(num):
-    cache = {1: 1, 2: 1, 3: 1}
-    def rec_trib(num):
-        result = cache.get(num)
-        if result is None:
-            result = rec_trib(num - 1) + rec_trib(num - 2) + rec_trib(num - 3)
-            cache[num] = result
-
-        print(cache)
-        return result
-    return rec_trib(num)
+def is_palindrome(string):
+    if len(string) < 2:
+        return True
+    else:
+        if string[0] == string[-1]:
+            return is_palindrome(string[1:-1])
+    return False
 
 
-print(tribonacci(30))
-
-
+print(is_palindrome('stepik'))
+print(is_palindrome('level'))
+print(is_palindrome('122333221'))
+print(is_palindrome('b'))
