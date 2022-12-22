e9abfa6c886fe833c504e5ecdbacbbb6811831d9
petr <pnikono24@gmail.com>
2022-12-22 18:14:46 +0300

    [upd] change 1 file

---
+++ codewars.py
@@ -0,0 +1,5 @@
+def litres(time):
+    return int(0.5 * time)
+
+
+print(litres(12.3))
--- example_2.py
+++ example_2.py
@@ -1,15 +1,14 @@
-def get_value(dt, key):
-    if key in dt:
-        return dt[key]
-    for k, v in dt.items():
-        if isinstance(v, dict):
-            value = get_value(v, key)
-            if value is not None:
-                return value
+def get_all_values(d, key):
+    st = set()
+    if key in d:
+        st.add(d[key])  # базовый случай
+    for k, v in d.items():
+        if type(v) == dict:
+            st.update(get_all_values(v, key))  # рекурсивный случай
+    return st
 
+my_dict = {'users': {'Arthur': {'grades': [4, 4, 3], 'top_grade': 4}, 'Timur': {'grades': [5, 5, 5], 'top_grade': 5}}}
+result = get_all_values(my_dict, 'top_grade')
 
+print(result)
 
-
-data = {'firstName': 'Тимур', 'lastName': 'Гуев', 'birthDate': {'day': 10, 'month': 'October', 'year': 1993}, 'address': {'streetAddress': 'Часовая 25, кв. 127', 'city': {'region': 'Московская область', 'type': 'город', 'cityName': 'Москва'}, 'postalCode': '125315'}}
-
-print(get_value(data, 'cityName'))
