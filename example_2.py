6034b5b56af0043052aa481aa7a751a8b46175db
petr <pnikono24@gmail.com>
2022-12-22 10:18:16 +0300

    [upd] change 1 file

--- example_2.py
+++ example_2.py
@@ -1,13 +1,17 @@
-def is_palindrome(string):
-    if len(string) < 2:
-        return True
-    else:
-        if string[0] == string[-1]:
-            return is_palindrome(string[1:-1])
-    return False
+def recursive_sum(ls):
+    if isinstance(ls, int):
+        return ls  # базовый случай
+    if isinstance(ls, list):
+        total = 0
+        for i in ls:
+            value = recursive_sum(i)
+            if value is not None:
+                total += value
+            else:
+                total += 0
+        return total
 
 
-print(is_palindrome('stepik'))
-print(is_palindrome('level'))
-print(is_palindrome('122333221'))
-print(is_palindrome('b'))
+my_list = [1, [4, 4], 2, [1, [2, 10]]]
+
+print(recursive_sum(my_list))
