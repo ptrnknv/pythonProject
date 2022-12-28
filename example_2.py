ea60f7eb080bb5535cf01c368cd3208550231c04
petr <pnikono24@gmail.com>
2022-12-28 17:21:46 +0300

    [upd] change 1 file

--- example_2.py
+++ example_2.py
@@ -1,14 +1,25 @@
-def get_all_values(d, key):
-    st = set()
-    if key in d:
-        st.add(d[key])  # базовый случай
-    for k, v in d.items():
-        if type(v) == dict:
-            st.update(get_all_values(v, key))  # рекурсивный случай
-    return st
+from string import ascii_lowercase as asc, digits as dg
 
-my_dict = {'users': {'Arthur': {'grades': [4, 4, 3], 'top_grade': 4}, 'Timur': {'grades': [5, 5, 5], 'top_grade': 5}}}
-result = get_all_values(my_dict, 'top_grade')
 
-print(result)
+def verification(login, password, success, failure):
+    dt = {
+        lambda x: x in asc or x in asc.upper(): 'в пароле нет ни одной буквы',
+        lambda x: x in asc.upper(): 'в пароле нет ни одной заглавной буквы',
+        lambda x: x in asc: 'в пароле нет ни одной строчной буквы',
+        lambda x: x in dg: 'в пароле нет ни одной цифры'
+    }
+    for f in dt:
+        if not any((f(l) for l in password)):
+            return failure(login, dt[f])
+    success(login)
 
+
+def success(login):
+    print(f'Здравствуйте, {login}!')
+
+
+def failure(login, text):
+    print(f'{login}, попробуйте снова. Текст ошибки: {text}')
+
+
+verification('Arthur_Davletov', 'мойпарольBEE123', success, failure)
