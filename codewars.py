39c4d59eeb740c8d5d8a9176c6c6e9fada0c58db
petr <pnikono24@gmail.com>
2023-02-21 11:14:41 +0300

    [upd] change 1 file

--- codewars.py
+++
@@ -1,5 +0,0 @@
-def litres(time):
-    return int(0.5 * time)
-
-
-print(litres(12.3))
--- example_2.py
+++ example_2.py
@@ -1,42 +1,34 @@
-from collections import namedtuple
-import itertools as it
+import xlrd
 
-Item = namedtuple('Item', ['name', 'mass', 'price'])
+wb = xlrd.open_workbook('trekking3.xlsx')
+sh = wb.sheet_by_index(0)
+ss = wb.sheet_by_index(1)
+common_dt = {}
+for rx in range(1, sh.nrows):
+    dd = sh.row(rx)
+    added_list = []
+    for el in dd[1:]:
+        el = el.value
+        if el== '':
+            el = 0
+        added_list.append(el)
+    common_dt[dd[0].value] = common_dt.get(dd[0].value, []) + added_list
+raskl = {}
+days = []
+for el in range(1, ss.nrows):
+    data = ss.row(el)
+    day = int(data[0].value)
+    name = data[1].value
+    gr = int(data[2].value)
+    days.append((day, name, gr))
+res = {}
+for k, *v in days:
+    if v[0] in common_dt:
+        temp = []
+        for el in common_dt[v[0]]:
+            temp.append(el * (v[1] / 100))
+        res[k] = res.get(k, []) + [temp]
+for el in res.values():
+    print(*map(int, map(sum, zip(*el))))
 
-items = [Item('Обручальное кольцо', 7, 49_000),
-         Item('Мобильный телефон', 200, 110_000),
-         Item('Ноутбук', 2000, 150_000),
-         Item('Ручка Паркер', 20, 37_000),
-         Item('Статуэтка Оскар', 4000, 28_000),
-         Item('Наушники', 150, 11_000),
-         Item('Гитара', 1500, 32_000),
-         Item('Золотая монета', 8, 140_000),
-         Item('Фотоаппарат', 720, 79_000),
-         Item('Лимитированные кроссовки', 300, 80_000)]
 
-weight_limit = int(input())
-mx_price = 0
-res = []
-for i in range(len(items)):
-    for j in it.combinations(items, i):
-        total_mass = 0
-        total_price = 0
-        name = ''
-        for el in j:
-            total_mass += el.mass
-            total_price += el.price
-            name = el.name
-        if total_mass <= weight_limit:
-            if total_price > mx_price:
-                mx_price = total_price
-                res.append(j)
-                print(total_mass)
-                print(total_price)
-
-for i in res:
-    print(sorted(i))
-if res:
-    for el in sorted(res[-1]):
-        print(el.name)
-else:
-    print('Рюкзак собрать не удастся')
