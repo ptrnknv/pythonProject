55fa6b2eab1383417424750058a812a660ad735c
petr <pnikono24@gmail.com>
2023-02-24 10:38:05 +0300

    [upd] change 1 file

--- example_2.py
+++ example_2.py
@@ -1,34 +1,36 @@
-import xlrd
-
-wb = xlrd.open_workbook('trekking3.xlsx')
-sh = wb.sheet_by_index(0)
-ss = wb.sheet_by_index(1)
-common_dt = {}
-for rx in range(1, sh.nrows):
-    dd = sh.row(rx)
-    added_list = []
-    for el in dd[1:]:
-        el = el.value
-        if el== '':
-            el = 0
-        added_list.append(el)
-    common_dt[dd[0].value] = common_dt.get(dd[0].value, []) + added_list
-raskl = {}
-days = []
-for el in range(1, ss.nrows):
-    data = ss.row(el)
-    day = int(data[0].value)
-    name = data[1].value
-    gr = int(data[2].value)
-    days.append((day, name, gr))
-res = {}
-for k, *v in days:
-    if v[0] in common_dt:
-        temp = []
-        for el in common_dt[v[0]]:
-            temp.append(el * (v[1] / 100))
-        res[k] = res.get(k, []) + [temp]
-for el in res.values():
-    print(*map(int, map(sum, zip(*el))))
+import requests
+import os
+import zipfile
+from openpyxl import load_workbook
 
 
+def download_url(url, save_path, chunk_size=128):
+    r = requests.get(url, stream=True)
+    with open(save_path, 'wb') as fd:
+        for chunk in r.iter_content(chunk_size=chunk_size):
+            fd.write(chunk)
+
+
+path_zip = '/home/petr/Desktop/PycharmProjects/pythonProject/task.zip'
+path_unzip = '/home/petr/Desktop/PycharmProjects/pythonProject/task/'
+if not os.path.isfile(path_zip):
+    url = 'https://stepik.org/media/attachments/lesson/245299/rogaikopyta.zip'
+    download_url(url, path_zip)
+
+if not os.path.isfile(path_unzip):
+    with zipfile.ZipFile(path_zip, "r") as zip_ref:
+        zip_ref.extractall(path_unzip)
+
+directory = os.fsencode(path_unzip)
+workers = []
+for file in os.listdir(directory):
+    filename = os.fsdecode(file)
+    workbook = load_workbook(filename=path_unzip + filename)
+    sheet = workbook.active
+    for value in sheet.iter_rows(min_row=1, max_row=3, values_only=True):
+        if value[0] == 'ФИО':
+            workers.append(value)
+
+with open('result.txt', 'w', encoding='utf8') as file:
+    for worker in sorted(workers, key=lambda x: x[1]):
+        file.write(f'{worker[1]} {worker[3]}\n')
